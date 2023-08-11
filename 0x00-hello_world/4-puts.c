376088f90edef8189cb8e9127b537e5092a45c91
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-11 10:26:32 +0300

    17

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ * Description: function to output a string
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
