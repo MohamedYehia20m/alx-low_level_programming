2c634fb49cbe8b74b1941b2fecce6af5f2e0d74c
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-11 10:42:45 +0300

    19

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+*main - entry point
+*Description:print the size of various types
+*Return:Always 0 Success
+*/
+int main(void)
+{
+	printf("Size of a char:%d", sizeof(char));
+	printf("Size of a int:%d", sizeof(int));
+	printf("Size of a long int:%d", sizeof(long int));
+	printf("Size of a long long int:%d", sizeof(long long int));
+	printf("Size of a float:%d", sizeof(float));
+	return (0);
+}
