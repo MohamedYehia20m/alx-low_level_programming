985391a2cb18582854316eed83969a1b15714b51
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-11 11:11:38 +0300

    23

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+*main - entry point
+*Desciption:print a string 
+*Return:Always 1 Failure 
+*/
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char str1[58] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	write(1,str1,58);
+	return (1);
+}
