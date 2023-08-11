07c966d099a96dae4142e03304c21c2b57ab10b0
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-11 11:29:43 +0300

    28

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 #include <stdlib.h>
 int main(void)
 {
-	system("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
 	return (1);
 }
