3dfecf856248781728f8afa3f3e04ba31d1626a2
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-15 14:20:03 +0300

    3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 /**
 *main - entry point
-*
-*Description : simple function to print string
-*
-*Return : Always 0 Success
+*Decription:Function to output a string
+*Return:Always 0  Success
 */
+
 #include "main.h"
 
 int main(void)
 {
 	char arr[10] =  "_putchar\n";
 	int i = 0;
+
 	while (arr[i] != '\0')
 	{
 		_putchar(arr[i++]);
