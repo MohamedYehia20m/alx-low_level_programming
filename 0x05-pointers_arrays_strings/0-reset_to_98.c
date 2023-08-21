15fdae328a5d51cb9040b9bf41d873dadbced972
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-22 00:16:27 +0300

    1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - reset value to 98
+ * @n : pointer to int
+ *
+ * Description: set the value of variable pointed by *n pointer to 98
+ * by dereferencing it
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
