da5ba6201c4e8612a30b76cb4dc162b7887346cb
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-24 01:20:20 +0300

    8

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,15 +14,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
+	while (*(s1 + i) == *(s2 + i))
 	{
+		if (*(s1 + i) == '\0')
+			return (0);
 		i++;
 	}
-
-	if (*(s1 + i) != '\0')
-		return (15);
-	else if (*(s2 + i) != '\0')
-		return (-15);
-	else
-		return (0);
+	return (*(s1 + i) - *(s2 + i));
 }
