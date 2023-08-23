fec602779b5287ce733af62a678026a7bc9adae4
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-24 00:04:54 +0300

    4

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+/**
+ * _strncpy - a function that copies a string.
+ * @src  : pointer to char.
+ * @dest : pointer to char.
+ * @n    : no. of bytes to be concatenated
+ *
+ * Description: a function that copies a string.
+ * by looping through the src parameter to copy n of bytes to dest
+ *
+ * Return: returns a pointer to char.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0' && i != n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (*(src + i) != '0')
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
