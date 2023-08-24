ae49984549bd96d0c2adaf306daba3dee1310a88
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-25 01:46:33 +0300

    12

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,37 @@
 /**
- * cap_string - a function that capitalizes all words of a string.
+ * cap_string -  a function that capitalizes all words of a string.
  * @s : pointer to char
  *
- * Description: a function that capitalizes all words of a string.
- * by looping through the string and capitalizes each character
+ * Description : a function that capitalizes all words of a string.
  *
  * Return: pointer to char
  */
-
 char *cap_string(char *s)
 {
-	int i = 0;
+	int separator[] = {  ' ', '\t', '\n',  ';'
+				,  '.',  '!',  '?',  '"'
+				,  '(',  ')',  '{',  '}' };
+	int i = 1;
+	int j = 0;
 
+	if ((*s >= 'a') && (*s <= 'z'))
+		*s -= 32;
 	while (*(s + i) != '\0')
 	{
-		if (*(s + i) >= 65 && *(s + i) <= 90)
-			*(s + i) = *(s + i) + 32;
-		if (*(s + i) == '\t')
-			*(s + i) = ' ';
+		while (j < 12)
+		{
+			if (*(s + i) == separator[j] &&
+				((*(s + i + 1) >= 'a') &&
+				 (*(s + i + 1) <= 'z')))
+			{
+				*(s + i + 1) -= 32;
+				break;
+			}
+			j++;
+		}
+		j = 0;
 		i++;
 	}
 	return (s);
 }
+
