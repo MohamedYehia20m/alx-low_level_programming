d48c383fc957f1c67ac32834d8abfbc80c5e0c1a
MohamedYehia20m <medoyehia2001@gmail.com>
2023-08-29 00:34:07 +0300

    6

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,13 +14,10 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (*(s + i) != '\0')
+	while (*(s++) != '\0')
 	{
-		if (*(s + i) == c)
-			return (s + i);
-		i++;
+		if (*(s) == c)
+			return (s);
 	}
 	return ('\0');
 }
