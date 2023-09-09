39ec82309f057e702bbf9bc22eb14a5015aa7204
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-09 21:28:53 +0300

    7

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * is_digit - checks if character is numeric or not
+ * @c: character to be tested
+ *
+ * Description: checks if character is numeric or not
+ *
+ * Return: 0 for non-numeric , 1 for numeric.
+ */
+
+int is_digit(char c)
+{
+	return (isdigit(c));
+}
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: argc is the number of arguments being passed into your program from
+ * the command line
+ * @argv: rgv stands for "argument vector".argv is a one-dimensional array of
+ * strings. Each string is one of the arguments that was passed to the
+ * program.
+ *
+ * Description: a program that multiplies two numbers.
+ *
+ * Return: return 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	while (i < argc)
+	{
+		if (is_digit(*(argv[i])) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", (sum));
+	return (0);
+}
