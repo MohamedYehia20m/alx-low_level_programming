09401f85baeec1af077c86b3fb65999aadfbb65c
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-09 19:41:24 +0300

    3

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - a program that prints  the number of arguments passed into it.
+ * @argc: argc is the number of arguments being passed into your program from
+ * the command line
+ * @argv: rgv stands for "argument vector".argv is a one-dimensional array of
+ * strings. Each string is one of the arguments that was passed to the
+ * program.
+ *
+ * Description: a program that prints the number of arguments passed into it.
+ *
+ * Return: return 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+        (void)(argv);
+        printf("%d\n", (argc - 1));
+        return (0);
+}
