ef70f02940e7b85147da59e46ef4038e827a1ea6
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-09 18:26:43 +0300

    2

--- 0x09-static_libraries/_isupper.c
+++ 0x09-static_libraries/_isupper.c
@@ -1,23 +1,11 @@
-/**
- * string_toupper - a function that changes all lowercase letters of a string
- * to uppercase.
- * @s : pointer to char
- *
- * Description:  a function that changes all lowercase letters of a string
- * to uppercase by looping thrpugh the string and upercasing every lowercase
- * alphabet.
- *
- * Return: pointer to char
- */
-char *string_toupper(char *s)
-{
-	int i = 0;
+#include "main.h"
 
-	while (*(s + i) != '\0')
-	{
-		if (*(s + i) >= 97 && *(s + i) <= 122)
-			*(s + i) = *(s + i) - 32;
-		i++;
-	}
-	return (s);
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
 }
