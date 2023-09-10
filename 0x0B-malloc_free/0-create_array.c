4309590af2b2a3bbb741d4e7238450c34bee7b95
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-10 18:58:33 +0300

    4

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,21 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: character to print
+ *
+ * Description: write the character c to stdout
+ *
+ * Return: on succees 1 , -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * create_array - a function that creates an array of chars, and initializes
@@ -25,10 +40,10 @@ char *create_array(unsigned int size, char c)
 		s = malloc(size * sizeof(char));
 	while (j < size)
 	{
-		*(s + j) = 'H';
+		*(s + j) = c;
 		j++;
 	}
 	if (s == NULL)
-		return (1);
+		_putchar('F');
 	return (s);
 }
