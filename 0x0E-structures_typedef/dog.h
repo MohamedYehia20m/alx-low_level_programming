72d29ab5802000feca475546807593118c851d08
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-11 20:22:02 +0300

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - create a struct for type dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: create a struct for type dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
