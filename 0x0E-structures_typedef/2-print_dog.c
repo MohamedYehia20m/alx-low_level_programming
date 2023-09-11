2acf9480b8330b4833b1dc55d24a6f42cdf684b5
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-11 21:20:39 +0300

    4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,6 +18,8 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("(nil)\n");
 	printf("Name: %s\n", d->name);
+	if (d->age == 0.0)
+		printf("(nil)\n");
 	printf("Age: %f\n", d->age);
 	if (d->name == NULL)
 		printf("(nil)\n");
