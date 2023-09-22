9ceb1f3204774fc4acc6c6c2404bc6654e24c783
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-22 18:36:01 +0300

    returning 0 for null string

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,10 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+			return (0);
+		}
 		printf("[%u] %s\n", h->len, h->str);
 		a++;
 		h = h->next;
