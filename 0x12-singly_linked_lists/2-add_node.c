e60b18dca464474d435ab61d03f26fe8a2bd65e4
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-22 18:29:23 +0300

    handling the output of malloc()

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -38,6 +38,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
 	new->len = _strlen(str);
 	new->next = *head;
